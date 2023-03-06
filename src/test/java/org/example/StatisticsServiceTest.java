b055698a497413f7217dac5c274f8c8764f45596
Kirill Igorevich <k.beketoff@yandex.ru>
2023-03-06 18:49:43 +0300

    correction

--- src/test/java/org/example/StatisticsServiceTest.java
+++ src/test/java/org/example/StatisticsServiceTest.java
@@ -1,19 +1,22 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
 import org.junit.jupiter.api.Assertions;
-import org.testng.annotations.Test;
 
-public class StatisticsServiceTest {
+    public class StatisticsServiceTest {
 
-    @Test
-    void findMax() {
-        StatisticsService service = new StatisticsService();
+        @Test
+        void findMax() {
+            StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
+            long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+            long expected = 12;
 
-        long actual = service.findMax(incomesInBillions);
+            long actual = service.findMax(incomesInBillions);
 
-        Assertions.assertEquals(expected, actual);
+            Assertions.assertEquals(expected, actual);
+        }
     }
+
 }
